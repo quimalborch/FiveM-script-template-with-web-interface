65b7f4419aeac3925eab79e215ca0be51fd89c9f
quimalborch <68965769+quimalborch@users.noreply.github.com>
2020-10-08 09:38:29 +0200

    Añadido la carpeta de HTML

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+client_script('client/client.lua') --your NUI Lua File
+
+ui_page('client/html/index.html') --THIS IS IMPORTENT
+
+--[[The following is for the files which are need for you UI (like, pictures, the HTML file, css and so on) ]]--
+files({
+    'client/html/index.html',
+    'client/html/script.js',
+    'client/html/style.css',
+})
